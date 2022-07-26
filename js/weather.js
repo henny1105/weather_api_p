e3c95ae7df41b861e90ae6659bfdfd4cef90738c
kimhyein <kimhyein1105@gmail.com>
2022-07-26 21:44:08 +0900

    수정

--- js/weather.js
+++ js/weather.js
@@ -3,27 +3,27 @@ var city = ['seoul', 'tokyo', 'paris', 'beijing'];
 
 callData();
 
-
 function callData() {
-    city.forEach(function (item, index) {
-        var apiURI = "http://api.openweathermap.org/data/2.5/weather?q=" + city[index] + "&appid=" + "84bbf1bc16c21571bb35b7478e7b2d24";
+    for (var i = 0; i < city.length; i++) {
+        var apiKey = "84bbf1bc16c21571bb35b7478e7b2d24" 
+        var apiURI = "http://api.openweathermap.org/data/2.5/weather?q=" + city[i] + "&appid=" + apiKey;
 
-        $.ajax({
-            url: apiURI,
-            dataType: "json",
-            type: "GET",
-            success: function (data) {
-                console.log(data);
-                createList(data);
-            },
-            error: function (error) {
-                console.error(error);
-            }
-        })
-    });
+        (function (i) {
+            $.ajax({
+                url: apiURI,
+                dataType: "json",
+                type: "GET",
+                success: function (data) {
+                    console.log(data);
+                    createList(data);
+                },
+                error: function (error) {
+                    console.error(error);
+                }
+            })
+        })(i);
+    }
 }
-
-
 
 function createList(data) {
     var city = data.name;
@@ -37,20 +37,85 @@ function createList(data) {
     $("html컴포넌트").attr("src", imgSrc);
 
 
-    console.log(city)
-        // $(".temp span").text(`${Math.floor(tit)}°`);
-        // $(".weather span").text(weather);
-        // $(".weather_desc span").text(weather_desc);
-        // $(".humidity span").text(humidity);
+    if (city == 'Seoul') {
+        $(".seoul .city").text(city);
+        $(".seoul .temp span").text(`${Math.floor(tit)}°`);
+        $(".seoul .weather span").text(weather);
+        $(".seoul .weather_desc span").text(weather_desc);
+        $(".seoul .humidity span").text(humidity);
 
-        // $(".wind span").text(wind);
-        // $(".cloudy span").text(cloudy);
-        // $(".imgSrc span").css({
-        //     backgroundImage: "url(" + imgSrc + ")",
-        //     width: '50px',
-        //     height: '50px',
-        //     display: 'inline-block'
-        // })
+        $(".seoul .wind span").text(wind);
+        $(".seoul .cloudy span").text(cloudy);
+        $(".seoul .imgSrc span").css({
+            backgroundImage: "url(" + imgSrc + ")",
+            width: '50px',
+            height: '50px',
+            display: 'inline-block'
+        })
+    }else if(city == 'Tokyo') {
+        $(".tokyo .city").text(city);
+        $(".tokyo .temp span").text(`${Math.floor(tit)}°`);
+        $(".tokyo .weather span").text(weather);
+        $(".tokyo .weather_desc span").text(weather_desc);
+        $(".tokyo .humidity span").text(humidity);
+
+        $(".tokyo .wind span").text(wind);
+        $(".tokyo .cloudy span").text(cloudy);
+        $(".tokyo .imgSrc span").css({
+            backgroundImage: "url(" + imgSrc + ")",
+            width: '50px',
+            height: '50px',
+            display: 'inline-block'
+        })
+    }else if(city == "Beijing") {
+        $(".beijing .city").text(city);
+        $(".beijing .temp span").text(`${Math.floor(tit)}°`);
+        $(".beijing .weather span").text(weather);
+        $(".beijing .weather_desc span").text(weather_desc);
+        $(".beijing .humidity span").text(humidity);
+
+        $(".beijing .wind span").text(wind);
+        $(".beijing .cloudy span").text(cloudy);
+        $(".beijing .imgSrc span").css({
+            backgroundImage: "url(" + imgSrc + ")",
+            width: '50px',
+            height: '50px',
+            display: 'inline-block'
+        })
+    }else if(city == "Paris") {
+        $(".paris .city").text(city);
+        $(".paris .temp span").text(`${Math.floor(tit)}°`);
+        $(".paris .weather span").text(weather);
+        $(".paris .weather_desc span").text(weather_desc);
+        $(".paris .humidity span").text(humidity);
+
+        $(".paris .wind span").text(wind);
+        $(".paris .cloudy span").text(cloudy);
+        $(".paris .imgSrc span").css({
+            backgroundImage: "url(" + imgSrc + ")",
+            width: '50px',
+            height: '50px',
+            display: 'inline-block'
+        })
+    }
+
+
+
+
+
+    // $(".temp span").text(`${Math.floor(tit)}°`);
+    // $(".weather span").text(weather);
+    // $(".weather_desc span").text(weather_desc);
+    // $(".humidity span").text(humidity);
+
+    // $(".wind span").text(wind);
+    // $(".cloudy span").text(cloudy);
+    // $(".imgSrc span").css({
+    //     backgroundImage: "url(" + imgSrc + ")",
+    //     width: '50px',
+    //     height: '50px',
+    //     display: 'inline-block'
+    // })
 
 
 
